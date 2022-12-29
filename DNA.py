ac4f0ac48ed6fcc975e000f01d94576319496166
Adriana Acero Fernández-Villacañas <91780726+AdrianaAceroFV@users.noreply.github.com>
2022-12-29 14:12:06 +0100

    Update DNA.py

--- DNA.py
+++ DNA.py
@@ -1,3 +1,11 @@
+# A string is simply an ordered collection of symbols selected from some alphabet and formed into a word; 
+# the length of a string is the number of symbols that it contains. An example of a length 21 DNA string 
+# (whose alphabet contains the symbols 'A', 'C', 'G', and 'T') is "ATGCTTCAGAAAGGTCTTACG."
+
+# Given: A DNA string s of length at most 1000 nt.
+
+# Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
+
 def counting_nt(s):
     if len(s) <= 1000:
         if set(s)=={'A','C','G','T'}:
@@ -24,7 +32,3 @@ def counting_nt2(s):
             print("Invalid nucleotides in DNA string")
     else:
         print("DNA string with more than 1000 nucleotides")
-
-
-
-
